b7e8d314df2681914898f67e2b6a89d6ef5438ee
Juho Turpeinen <turpeinenjuho@gmail.com>
2020-01-31 23:06:53 +0200

    Simple movement script. ProBuilder initialization.

---
+++ Assets/Scripts/Data/SavedPlayerFrame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public struct SavedPlayerFrame
+{
+    public readonly Vector3 Position;
+    public readonly Quaternion Rotation;
+
+    public SavedPlayerFrame(Vector3 position, Quaternion rotation)
+    {
+        Position = position;
+        Rotation = rotation;
+    }
+}
---
+++ Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] [Range(0.1f, 5f)] private float moveSpeed = 1f;
+    [SerializeField] [Range(0.1f, 5f)] private float turnSpeed = 1f;
+    
+    private CharacterController characterController;
+    private Queue<SavedPlayerFrame> savedPlayerFrames;
+    private float horizontal;
+    private float vertical;
+
+    private const string HorizontalAxis = "Horizontal";
+    private const string VerticalAxis = "Vertical";
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        savedPlayerFrames = new Queue<SavedPlayerFrame>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxisRaw(HorizontalAxis);
+        vertical = Input.GetAxisRaw(VerticalAxis);
+        Vector3 movement = new Vector3(horizontal, 0, vertical).normalized * Time.deltaTime * moveSpeed;
+        characterController.Move(movement);
+        
+        if (Mathf.Abs(horizontal + vertical) > 0)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        SavedPlayerFrame savedPlayerFrame = new SavedPlayerFrame(transform.position, transform.rotation);
+        savedPlayerFrames.Enqueue(savedPlayerFrame);
+    }
+}
